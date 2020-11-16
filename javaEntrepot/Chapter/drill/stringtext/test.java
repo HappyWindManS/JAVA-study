af012e36013a1c0c628754bb33e2d8e3cb90956f
MR_QIN <3066004354@qq.com>
2020-11-17 00:29:01 +0800

    JAVA

---
+++ javaEntrepot/Chapter/drill/stringtext/test.java
@@ -0,0 +1,13 @@
+package stringtext;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String[] array = new String[1];
+		array[0] = "123";
+		String str = "123";
+		System.out.println(array[0]==str);
+	}
+
+}
