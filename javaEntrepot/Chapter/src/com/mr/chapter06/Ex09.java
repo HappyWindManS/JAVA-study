5d8770a2d3b8c01875ec6e653579030a019c345f
MR_QIN <3066004354@qq.com>
2020-11-20 12:16:13 +0800

    JAVA

--- javaEntrepot/Chapter/src/com/mr/chapter06/Ex09.java
+++ javaEntrepot/Chapter/src/com/mr/chapter06/Ex09.java
@@ -1,10 +1,18 @@
 package com.mr.chapter06;
 
+import java.util.HashSet;
+
 public class Ex09 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		HashSet set = new HashSet();
+		set.add("Jack");
+		set.add("Eve");
+		set.add("Rose");
+		set.add("Rose");
+		//lambda表达式
+		set.forEach(obj -> System.out.println(obj));
 	}
 
 }
