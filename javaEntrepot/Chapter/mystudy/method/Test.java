409632d4b52c7a20901b866aa8d55328472813fb
MR_QIN <3066004354@qq.com>
2020-12-27 00:13:49 +0800

    java

--- javaEntrepot/Chapter/mystudy/method/Test.java
+++ javaEntrepot/Chapter/mystudy/method/Test.java
@@ -13,6 +13,11 @@ public class Test {
 		      } while (i < 500);     
 		       System.out.println(count);   
 		       int e = 0;
+		       int d = e = 30;
+		       int b;
+		       int a = 10;
+		       String s = new String(new char {'i','t','c','s','t'});
+		       int[]arr= {1,2,23,4,5};
 		} 
 }
 
