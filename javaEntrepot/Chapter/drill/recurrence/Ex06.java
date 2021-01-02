c2458dfaa1232767a0482d076d37595a458971e2
MR_QIN <3066004354@qq.com>
2021-01-02 23:57:31 +0800

    java

--- javaEntrepot/Chapter/drill/recurrence/Ex06.java
+++ javaEntrepot/Chapter/drill/recurrence/Ex06.java
@@ -1,25 +1,79 @@
 package recurrence;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class Ex06 {
 
-	public static int[] array;
+	
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		Scanner input = new Scanner(System.in);
 		int value = input.nextInt();
 		int time = input.nextInt();
-		TreeSet<int[]> ts = new TreeSet<int[]>();
-		array = new int[time];
-		for(int i = 0,temp = value-time+1;i<temp;i++)
+		HashSet<String> hs = new HashSet<String>();
+		int[] array = new int[time];
+		int num = time-1;
+		//先初始化对照组
+		for(int i=0;i<time;i++)
 		{
-			for(int l=1;l<time;l++)
+			array[i] = 1;
+			if(i==num)
 			{
-				
+				array[num] = value;
+				for(int l=0;l<num;l++)
+				{
+					array[num] -= array[l];
+				}
 			}
 		}
+		Arrays.sort(array);	
+		String strTemp = "";
+		for(int i =0;i<time;i++)
+		{
+			strTemp += array[i]+",";
+		}
+		hs.add(strTemp);
+		for(int l=0;;l++)
+		{
+			int temp = 0;
+			strTemp = "";
+			for(int i=0;i<time;i++)
+			{
+				if(array[temp]<array[i])
+				{
+					temp = i;
+				}
+			}
+			for(int i = temp-1;i>=0;i--)
+			{ 	
+				array[temp]--;
+				if(array[i]==array[temp])
+				{
+					continue;
+				}
+				else
+				{
+					array[i]++;
+					break;
+				}
+			}
+			Arrays.sort(array);			
+			for(int i =0;i<time;i++)
+			{
+				strTemp += array[i]+",";
+			}
+			if(!hs.contains(strTemp))
+			{
+				hs.add(strTemp);
+			}
+			else
+			{
+				break;
+			}
+		}	
+		System.out.println(hs.size());
 	}
 	
 	
