6416d1fd56bc5f9923153bdb645eed48ac350c52
MR_QIN <3066004354@qq.com>
2021-01-25 21:07:43 +0800

    Java

--- javaEntrepot/Chapter/drill/recurrence/Ex02.java
+++ javaEntrepot/Chapter/drill/recurrence/Ex02.java
@@ -1,34 +1,68 @@
 package recurrence;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Ex02 {
 
+	public static int sum = 0;
+	
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		//一定是奇数个奇数的和才是奇数
-		//则，当有X个整数的时候，从中选出Y个数相加，得到的奇数一定是
-		// NUM = 奇数+偶数个偶数的和 OR 奇数个奇数的和
-		//则，需要Y个整数相加的时候
-		//奇数的条件一定为(奇数个奇数+偶数个偶数)
-		//----------------------------------------
-		//当存在4个数，任选三个时
-		//应有 sum = n(n%2=1)*j+m(m%2=0)*o
-		//sum =  
 		Scanner input = new Scanner(System.in);
-		int number = input.nextInt();
-		int time = input.nextInt();
-		int[] array = new int[number];
-		HashMap<String,Integer> hs = new HashMap<String,Integer>();
-		for(int i =0;i<number;i++)
+		int num = input.nextInt();
+		int size = input.nextInt();
+		int[][] temp = new int[num][2];
+		int[] array = new int[size];
+		
+		for(int i=0;i<num;i++)
 		{
-			array[i] = input.nextInt();
+			temp[i][0] = input.nextInt();
 		}
-		for(int i=0;i<number;i++)
+		input.close();
+		dfs(array,temp,0,0);
+		System.out.println(sum);
+	}
+	
+	public static void dfs(int[] array,int[][] temp,int pointer,int i)
+	{
+		if(pointer == array.length)
 		{
-			
+			int num = 0;
+			for(int i1 =0;i1<array.length;i1++)
+			{
+				num+= array[i1];
+			}
+			if(s(num))
+			{
+				sum++;
+			}
+			return;
 		}
+		for(;i<temp.length;i++)
+		{
+			if(temp.length - pointer >= array.length-1)
+			{
+				if(temp[i][1]!=1)
+				{
+					array[pointer] = temp[i][0];
+					temp[i][1] = 1;
+					dfs(array,temp,pointer+1,i);
+					temp[i][1] = 0;
+				}
+			}
+		}
+	}
+	
+	public static boolean s(int num)
+	{
+		for(int i=2;i<Math.sqrt(num)+1;i++)
+		{
+			if(num%i==0&&i!=num)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
